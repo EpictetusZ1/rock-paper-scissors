0dede14ae0c61ff8cec1b586bccbfa851817175f
J Heaton <jackrobertheaton@gmail.com>
2021-07-09 13:23:57 -0400

    Add user input function and result evaluation function. Fixed commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const choices = ["Rock", "Paper", "Scissors"]
+const choices = ["rock", "paper", "scissors"]
 
 function computerPlay(choices) {
-    let computerSelection = ""
+    let computerSelection
     let randomNum = Math.floor(Math.random() * 100)
     console.log(randomNum)
     if (randomNum < 33) {
@@ -11,7 +11,40 @@ function computerPlay(choices) {
     } else {
         computerSelection = choices[2]
     }
-    return console.log(computerSelection)
+    return computerSelection
 }
 
-computerPlay(choices)
+console.log(computerPlay(choices))
+
+function playerSelection() {
+    return document.getElementById("player-input").value.toLowerCase()
+}
+
+function gameResult(computerSelection, playerSelection) {
+    if (playerSelection === "rock" && computerSelection !== "paper") {
+        console.log(`Congratulations, You won! the Computer picked: ${computerSelection}`)
+    } else if (playerSelection === "paper" && computerSelection !== "scissors") {
+        console.log(`Congratulations, You won! the Computer picked: ${computerSelection}`)
+    } else if (playerSelection === "scissors" && computerSelection !== "rock") {
+        console.log(`Congratulations, You won! the Computer picked: ${computerSelection}`)
+    } else {
+        console.log(`You Lost! the Computer picked: ${computerSelection}. Better Luck Next Time!`)
+    }
+}
+
+gameResult(computerPlay(choices), playerSelection())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
