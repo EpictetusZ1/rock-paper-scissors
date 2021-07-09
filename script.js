1e94e2737a14819219790b380bf27a97c1fcba88
J Heaton <jackrobertheaton@gmail.com>
2021-07-09 15:23:13 -0400

    Adjust logic eval. for valid entries in game

--- script.js
+++ script.js
@@ -14,27 +14,33 @@ function computerPlay(choices) {
     return computerSelection
 }
 
-console.log(computerPlay(choices))
-
-function playerSelection() {
-    return document.getElementById("player-input").value.toLowerCase()
+function playerSelection(choices) {
+    let selection
+    selection = window.prompt("Type your choice here")
+    selection = selection.toLowerCase()
+    if (choices.indexOf(selection.toString()) === -1) {
+        selection = window.prompt("Not a valid entry, please choose one of the following: rock, paper, scissors")
+    } else {
+        console.log("The user input after prompt is " + selection)
+    }
+    return selection
 }
 
 function gameResult(computerSelection, playerSelection) {
-    if (playerSelection === "rock" && computerSelection !== "paper") {
+    if (computerSelection === playerSelection) {
+        console.log(`It's a Draw! You and the Computer Both Picked ${computerSelection}. Better Luck Next Time!`)
+    } else if (playerSelection === "rock" && computerSelection !== "paper") {
         console.log(`Congratulations, You won! the Computer picked: ${computerSelection}`)
     } else if (playerSelection === "paper" && computerSelection !== "scissors") {
         console.log(`Congratulations, You won! the Computer picked: ${computerSelection}`)
     } else if (playerSelection === "scissors" && computerSelection !== "rock") {
         console.log(`Congratulations, You won! the Computer picked: ${computerSelection}`)
-    } else {
+    }  else {
         console.log(`You Lost! the Computer picked: ${computerSelection}. Better Luck Next Time!`)
     }
 }
 
-gameResult(computerPlay(choices), playerSelection())
-
-
+gameResult(computerPlay(choices), playerSelection(choices))
 
 
 
